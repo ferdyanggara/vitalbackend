640efc1cc62a94a7729794e00028fe8b1f2ed37c
Tridib Mitra <tridibmitra@Tridibs-MacBook-Air.local>
2021-06-17 14:20:36 +0530

    implemented and tested image upload api

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "upload");
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+app.get("/", (req, res) => {
+  res.status(200).send("You can post to /upload.");
+});
+
+app.post("/upload", upload.single("image"), (req, res, next) => {
+  try {
+    return res.status(201).json({
+      message: "File uploded successfully",
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    `server is running at http://localhost:${process.env.PORT || 3000}`
+  );
+});
